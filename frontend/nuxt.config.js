9c9fdcdf00431e2f543a7aa3784a7afe65e8fe95
Emma Andersson <73166517+EmmAnders@users.noreply.github.com>
2022-05-08 19:17:37 +0200

    backend/api

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -36,8 +36,19 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ["@nuxtjs/axios"],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  publicRuntimeConfig: {
+    axios: {
+      baseURL:
+        /* process.env.PRODUCTION_API_URL || */ "http://localhost:1337/api",
+    },
+  },
+
+  strapi: {
+    url: "<http://localhost:1337>",
+  },
 };
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <Tutorial/>
+  <div>{{ about }}</div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+
+  async asyncData({ $axios }) {
+    try {
+      const [aboutRes, exhibitionsRes] = await Promise.all([
+        $axios.$get(`/about?populate=*`),
+        $axios.$get(`/exhibitions`),
+      ]);
+      return {
+        about: aboutRes.data.attributes,
+        exhibitions: exhibitionsRes.data,
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
 </script>
