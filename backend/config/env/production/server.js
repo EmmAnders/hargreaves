a5e97428fa9ddf64c78fb0f77fb87290fa89c427
Emma Andersson <73166517+EmmAnders@users.noreply.github.com>
2022-05-08 21:31:29 +0200

    change url

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env("https://enigmatic-plateau-23532.herokuapp.com/"),
+  url: env("https://gentle-harbor-08583.herokuapp.com/"),
   app: {
     keys: env.array("APP_KEYS"),
   },
