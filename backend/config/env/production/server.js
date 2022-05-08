ffabc02a46fc830a3ca776cf9878549d96060008
Emma Andersson <73166517+EmmAnders@users.noreply.github.com>
2022-05-08 21:43:34 +0200

    update

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env("https://gentle-harbor-08583.herokuapp.com/"),
+  url: env("https://gentle-harbor-08583.herokuapp.com"),
   app: {
     keys: env.array("APP_KEYS"),
   },
