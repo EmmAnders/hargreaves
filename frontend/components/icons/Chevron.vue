797d3c8cfb54dc35a9919421d72b75429ce6c3c9
Emma Andersson <73166517+EmmAnders@users.noreply.github.com>
2022-05-09 23:46:33 +0200

    add icons

---
+++ frontend/components/icons/Chevron.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="1"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-chevron-down"
+  >
+    <polyline points="6 9 12 15 18 9"></polyline>
+  </svg>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.chevron {
+  transition: 0.3s ease-out;
+}
+.chevron.rotate {
+  transition: transform 0.3s ease-in;
+  transform: rotate(180deg);
+}
+</style>
---
+++ frontend/components/icons/PlusMinus.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg
+    class="plus"
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path class="vertical-line" d="M16 8V24" stroke="currentColor" />
+    <path class="horizontal-line" d="M24 16L8 16" stroke="currentColor" />
+  </svg>
+</template>
+<script>
+export default {
+  name: "PlusMinusIcon",
+};
+</script>
+
+<style lang="scss">
+.plus {
+  height: 50px;
+  width: 30px;
+}
+.plus path {
+  transition: all 0.3s;
+  transform: scaleY(1);
+  transform-origin: 50% 50%;
+  stroke: var(--themeColor);
+}
+.plus.minus .vertical-line {
+  transform: scaleY(0);
+}
+</style>
