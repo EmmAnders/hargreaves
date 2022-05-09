7f13fd4882783d03d03b1518675ed9c78e63fe75
Emma Andersson <73166517+EmmAnders@users.noreply.github.com>
2022-05-09 18:58:20 +0200

    Update nuxt.config.js

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  // Globaal page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "Art by Hargreaves",
     htmlAttrs: {
