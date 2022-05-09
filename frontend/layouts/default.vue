39fc4c712a00888c9613ef37ffd49f6da6d2f6a3
Emma Andersson <73166517+EmmAnders@users.noreply.github.com>
2022-05-09 23:47:07 +0200

    update default

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,3 +1,37 @@
 <template>
-  <div><Nuxt /></div>
+  <div class="bg-theme-background text-theme-color h-full">
+    <Navigation class="z-50" />
+    <Nuxt class="h-full flex flex-col" />
+    <Wishlist />
+  </div>
 </template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+export default {
+  transition: "default",
+  data() {
+    return {
+      selected: false,
+    };
+  },
+  computed: {
+    ...mapState(["artPieces"]),
+  },
+  methods: {
+    ...mapActions(["fetchWishlist"]),
+  },
+  async mounted() {
+    this.$nextTick();
+    this.fetchWishlist();
+  },
+};
+</script>
+<style lang="scss">
+div#__nuxt,
+#__layout,
+#__layout > div,
+#app {
+  min-height: 100vh;
+}
+</style>
